4688644ae951fc4be7577308b8a2b0e69e612895
arthurbetto <obetto@sorint.com>
2026-09-21 19:08:58 +0200

    notification multi-language step3

--- test/feature/team/ui/widgets/team_clocking_requirement_section_test.dart
+++ test/feature/team/ui/widgets/team_clocking_requirement_section_test.dart
@@ -2,29 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:note_sondage/feature/team/ui/widgets/team_clocking_requirement_section.dart';
 
+import '../../../../support/test_app.dart';
+
 void main() {
   testWidgets('keeps AM/PM picker and sends an unchanged local HH:mm time', (
     tester,
   ) async {
     String? selected;
     await tester.pumpWidget(
-      MaterialApp(
-        home: Scaffold(
-          body: SingleChildScrollView(
-            child: TeamClockingRequirementSection(
-              clockingRequired: true,
-              onClockingRequiredChanged: (_) {},
-              requiredStartDate: '2026-09-16',
-              onRequiredStartDateChanged: (_) {},
-              requiredEndDate: null,
-              onRequiredEndDateChanged: (_) {},
-              reminderTime: '14:40',
-              onReminderTimeChanged: (value) => selected = value,
-              missingAlertTime: '14:45',
-              onMissingAlertTimeChanged: (_) {},
-              openAlertTime: '18:00',
-              onOpenAlertTimeChanged: (_) {},
-            ),
+      buildTestApp(
+        child: SingleChildScrollView(
+          child: TeamClockingRequirementSection(
+            clockingRequired: true,
+            onClockingRequiredChanged: (_) {},
+            requiredStartDate: '2026-09-16',
+            onRequiredStartDateChanged: (_) {},
+            requiredEndDate: null,
+            onRequiredEndDateChanged: (_) {},
+            reminderTime: '14:40',
+            onReminderTimeChanged: (value) => selected = value,
+            missingAlertTime: '14:45',
+            onMissingAlertTimeChanged: (_) {},
+            openAlertTime: '18:00',
+            onOpenAlertTimeChanged: (_) {},
           ),
         ),
       ),
